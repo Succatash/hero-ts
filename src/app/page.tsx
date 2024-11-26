795cf65308c378cda989bbf68599ba3943ea9176
succatash <mstein1284@gmail.com>
2024-11-26 17:38:32 -0500

    fixed margin issues on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <p className="h-30 mt-28 w-screen items-center">
+    <p className="h-30 w-screen items-center pt-28">
       Honouring those whose helped Israel from fighting on the battlefield to
       fighting in the political field. Israel poinrasndf;lkans;a ads f adf ads f
       asdf asdf a
